d0b79d66f7f20b870323591c0c411c39b6f596a0
Lazy22zz <Claudeleave@gmail.com>
2024-11-04 09:17:07 -0800

    Enable the gun architure

--- UE5_SimpleShooter/ShooterCharacter.cpp
+++ UE5_SimpleShooter/ShooterCharacter.cpp
@@ -41,6 +41,8 @@ void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 	PlayerInputComponent -> BindAction(TEXT("Jump"), EInputEvent::IE_Pressed, this, &AShooterCharacter::Jump);
 	PlayerInputComponent -> BindAxis (TEXT("LookUpRate"), this, &AShooterCharacter::LookUpRate );
 	PlayerInputComponent -> BindAxis (TEXT("LookRightRate"), this, &AShooterCharacter::LookRightRate);
+	PlayerInputComponent -> BindAction (TEXT("Shoot"), EInputEvent::IE_Pressed, this, &AShooterCharacter::Shoot);
+	
 }
 
 void AShooterCharacter::MoveForward(float AxisValue)
@@ -71,4 +73,9 @@ void AShooterCharacter::LookUpRate(float AxisValue)
 void AShooterCharacter::LookRightRate(float AxisValue)
 {
 	AddControllerYawInput(AxisValue * RotationRate * GetWorld() -> GetDeltaSeconds());
+}
+
+void AShooterCharacter::Shoot()
+{
+	Gun -> PullTrigger();
 }

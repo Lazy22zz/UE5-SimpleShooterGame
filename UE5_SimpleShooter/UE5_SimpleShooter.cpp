9a148113bf0029a2d17a6b33026e5d8c2ef2faec
Lazy22zz <Claudeleave@gmail.com>
2024-11-03 20:28:37 -0800

    Add files

---
+++ UE5_SimpleShooter/UE5_SimpleShooter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_SimpleShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_SimpleShooter, "UE5_SimpleShooter" );

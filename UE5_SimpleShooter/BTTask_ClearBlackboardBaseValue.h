282905429341fe442c8b2ced7e8079d721d9e317
Lazy22zz <Claudeleave@gmail.com>
2024-11-10 14:01:54 -0800

    Add the attask c++

---
+++ UE5_SimpleShooter/BTTask_ClearBlackboardBaseValue.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_ClearBlackboardBaseValue.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_SIMPLESHOOTER_API UBTTask_ClearBlackboardBaseValue : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+public:
+	UBTTask_ClearBlackboardBaseValue();
+};
